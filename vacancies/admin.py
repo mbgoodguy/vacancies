98dcb2ce71b16418b75201e2b89c6264c5c8e7a1
mb <nerooren4001@gmail.com>
2023-07-17 15:19:45 +0300

    Add new field to Vacancy model

--- vacancies/admin.py
+++ vacancies/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from vacancies.models import Vacancy
+
 # Register your models here.
+admin.site.register(Vacancy)
+
--- vacancies/models.py
+++ vacancies/models.py
@@ -2,4 +2,17 @@ from django.db import models
 
 
 class Vacancy(models.Model):
+    STATUS = [
+        ("draft", "Черновик"),  # второй аргумент - человекочитаемое значение для админки
+        ("open", "Открыта"),
+        ("closed", "Закрыта"),
+
+    ]
+
+    slug = models.SlugField(max_length=75)
     text = models.CharField(max_length=2000)
+    status = models.CharField(max_length=6, choices=STATUS, default="draft")
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.slug
