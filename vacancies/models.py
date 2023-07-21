2aeb64bb2c60e4834652860146a8ca43cd334736
mbgoodguy <nerooren4001@gmail.com>
2023-07-21 05:14:54 +0300

    Add user field into Vacancy model

--- vacancies/models.py
+++ vacancies/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
@@ -13,6 +14,7 @@ class Vacancy(models.Model):
     text = models.CharField(max_length=2000)
     status = models.CharField(max_length=6, choices=STATUS, default="draft")
     created = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.slug
